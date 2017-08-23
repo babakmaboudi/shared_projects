12b2cfa3ef0871873304e06786b07cde6a123c01
Ashish Bhatt <ashish.bhatt@mathematik.uni-stuttgart.de>
2017-08-23 19:53:46 +0200

    modified generate_snaps.py and wave.py to include energy preserving implicit_midpoint rule

--- elastic_reduced/generate_snaps.py
+++ elastic_reduced/generate_snaps.py
@@ -2,6 +2,6 @@ import wave as wv
 
 wave = wv.Wave()
 wave.initiate_fem()
-wave.strommer_verlet()
+wave.implicit_midpoint()
 wave.save_snapshots()
 wave.save_vtk_result()
--- elastic_reduced/wave.py
+++ elastic_reduced/wave.py
@@ -14,6 +14,9 @@ def epsilon(u):
 
 def sigma(u,lambda_,mu,d):
     return lambda_*nabla_div(u)*Identity(d) + 2*mu*epsilon(u)
+  
+def energy(q0,p0,f,lambda_,mu,d):
+    return 0.5*inner(p0,p0)*dx + 0.5*inner(sigma(q0,lambda_,mu,d),epsilon(q0))*dx - inner( f,q0 )*dx
 
 class Wave:
 	def __init__( self ):
@@ -29,8 +32,9 @@ class Wave:
 		self.g = self.gamma
 
 		# numerical parameters
-		self.MAX_ITER = 5000
+		self.MAX_ITER = 500
 		self.dt = 0.01
+		self.theta = 0.5;
 
 	def initiate_fem( self ):
 		#define mesh
@@ -55,12 +59,22 @@ class Wave:
 		#define right hand side function
 		self.f = Constant((0, 0, -self.rho*self.g))
 		self.T = Constant((0, 0, 0))
+		
+		#initialize the energy vector
+		self.e_vec = assemble(energy(self.q0,self.p0,self.f,self.lambda_,self.mu,self.d))
 
 		#define the weak form with the strommer-verlet scheme 
-		self.aq = inner(self.q,self.v)*dx
-		self.Lq = inner(self.q0,self.v)*dx + self.dt/2*inner(self.p0,self.v)*dx
+		#self.aq = inner(self.q,self.v)*dx
+		#self.Lq = inner(self.q0,self.v)*dx + self.dt/2*inner(self.p0,self.v)*dx
+		#self.ap = inner(self.p,self.v)*dx
+		#self.Lp = inner(self.p0,self.v)*dx -self.dt*inner(sigma(self.q_new,self.lambda_,self.mu,self.d),epsilon(self.v))*dx + self.dt*inner(self.f,self.v)*dx
+		
+		#define the weak form of implicit-midpoint scheme
+		self.aq = inner(self.q,self.v)*dx + pow(self.theta,2)*pow(self.dt,2)*inner( sigma(self.q,self.lambda_,self.mu,self.d) , epsilon(self.v) )*dx
+		self.Lq = inner(self.q0,self.v)*dx - pow(self.dt,2)*self.theta*(1-self.theta)*inner( sigma(self.q0,self.lambda_,self.mu,self.d) , epsilon(self.v) )*dx + self.dt*inner(self.p0,self.v)*dx + pow(self.dt,2)*pow(self.theta,2)*inner(self.f,self.v)*dx + pow(self.dt,2)*self.theta*(1-self.theta)*inner(self.f,self.v)*dx
 		self.ap = inner(self.p,self.v)*dx
-		self.Lp = inner(self.p0,self.v)*dx -self.dt*inner(sigma(self.q_new,self.lambda_,self.mu,self.d),epsilon(self.v))*dx + self.dt*inner(self.f,self.v)*dx
+		self.Lp = inner(self.p0,self.v)*dx - self.dt*self.theta*inner( sigma(self.q_new,self.lambda_,self.mu,self.d) , epsilon(self.v) )*dx - self.dt*(1-self.theta)*inner( sigma(self.q0,self.lambda_,self.mu,self.d) , epsilon(self.v) )*dx + self.dt*self.theta*inner(self.f,self.v)*dx + self.dt*(1-self.theta)*inner(self.f,self.v)*dx #+ dt*theta*inner(T, v)*ds + dt*(1-theta)*inner(T,v)*ds
+
 		self.Aq = assemble(self.aq)
 		self.Ap = assemble(self.ap)
 		self.Aq_mat = np.matrix( self.Aq.array() )
@@ -106,6 +120,30 @@ class Wave:
 
 			self.q0.assign( self.q_new )
 			
+#			if np.mod(i,50) == 0:
+			self.snap_Q = np.c_[self.snap_Q,coef]
+			
+	def implicit_midpoint( self ):
+		#store initial data
+		self.snap_Q = np.zeros((528,1))
+		self.snap_P = np.zeros((528,1))
+
+		#loop over time steps
+		for i in range(0,self.MAX_ITER):
+			print(i)
+			self.apply_bc_q()
+			coef = np.linalg.solve(self.Aq_mat,self.bq_mat)
+			self.q_new.vector().set_local( coef )
+
+			self.apply_bc_p()
+			coef = np.linalg.solve(self.Ap_mat,self.bp_mat)
+			self.p_new.vector().set_local( coef )
+
+			self.p0.assign( self.p_new )
+			self.q0.assign( self.q_new )
+
+#			if np.mod(i,50) == 0:
+			self.snap_P = np.c_[self.snap_P,coef]
 #			if np.mod(i,50) == 0:
 			self.snap_Q = np.c_[self.snap_Q,coef]
 
