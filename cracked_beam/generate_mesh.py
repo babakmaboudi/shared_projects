007afb9ca9d5f50a2ccbbeb76e2cf25adcd586ab
Ashish Bhatt <ashish.bhatt@mathematik.uni-stuttgart.de>
2018-02-05 19:39:32 +0100

    write code for geometries with nonuniform and coarse mesh

--- cracked_beam/generate_mesh.py
+++ cracked_beam/generate_mesh.py
@@ -7,28 +7,32 @@ from fenics import *
 from mshr import *
 from math import pi, sin, cos, sqrt
  
+L = 1
+W = 0.2
+w=L/100
+b1 = Box(Point(0, 0, 0), Point(L, W, W))
+b2 = Box(Point(L/2-w, 0, W/2), Point(L/2+w, W, W))
+#geometry = b1 - s2
+
 # Parameters
-R = 1.1
-r = 0.4
-t = 10
-x = R*cos(float(t) / 180 * pi)
-y = 0
+R = W/4
+r = 0.08
+t = W
+x = W/2+R*cos(float(t) / 180 * pi)
+y = W/2
 z = R*sin(t)
  
 # Create geometry
-s1 = Sphere(Point(0, 0, 0), 1)
+s1 = Sphere(Point(x+L-3/2*W, y, z), r)
 s2 = Sphere(Point(x, y, z), r)
-b1 = Box(Point(-2, -2, -0.03), Point(2, 2, 0.03))
-geometry = s1 - s2 - b1
+#b1 = Box(Point(-2, -2, -0.03), Point(2, 2, 0.03))
+geometry = b1 - s2 -s1
 
-L = 1
-W = 0.2
-w=L/500
-b1 = Box(Point(0, 0, 0), Point(L, W, W))
-b2 = Box(Point(L/2-w, 0, W/2), Point(L/2+w, W, W))
-geometry = b1 - b2
- 
+#r1 = Rectangle(Point(0,0),Point(L,W))
+#r2 = Rectangle(Point(L/2-w,W/2),Point(L/2+w,W))
+#geometry = r1-r2
+
 # Create mesh
-mesh = generate_mesh(geometry,32)
+mesh = generate_mesh(geometry,10)
 
-File("cracked_beam.pvd") << mesh
+File("results/test.pvd") << mesh
